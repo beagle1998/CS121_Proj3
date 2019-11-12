d8e9eac4af30226613b49243813ab7fd4510ed9b
Andrew Nguyen <andrewtnguyen49@gmail.com>
2019-11-12 12:57:10 -0800

    Finished Parsing the lines from index

--- venv/SearchAndRetrieval.py
+++ venv/SearchAndRetrieval.py
@@ -1,36 +1,50 @@
 
 #Search Retrivel Component
 
-#Query 1 "I love pie"
-#Add 
+#Query 1 
+#Steps:
+
+#	1) Type in Search:
+#	"cristina lopes"
+
+#	2) Get Postings Dictionary
+#	cristina lopes 
+#	{"cristina": [['DocID=2085', 'Pos=[16772, 16791]', 'Count=2', 'TDIF=2.0']],
+#   "lopes": [['DocID=2085', 'Pos=[16773, 16791]', 'Count=2', 'TDIF=2.0']]}
+
+#	3) Merge Postings from each item in dictionary by Document ID
 
 #Retrieve Posting Lists
 def retrievePostingList(queryStr):
+	
 	#Open Document
 	f1 = open("./INDEX.txt","r",errors="ignore")
+	
 	#Split query by space
 	listQuery = queryStr.split()
+	
 	#Create a dictionary to hold postings of each word { word-str:Postings-list }
-	dictPostings =  dict() #cristina lopes {"cristina": [['DocID=2085', 'Pos=[16772, 16791]', 'Count=2', 'TDIF=2.0']],
-						   # "lopes": [['DocID=2085', 'Pos=[16773, 16791]', 'Count=2', 'TDIF=2.0']]}
-						   #
+	dictPostings =  dict() 
+	
 	#Build dictPostings by finding the postings for each word 
 	for word in listQuery:
 		while True:
-			if(parseLineFromWord(f1.readline()) == word):
-				dictPostings[word] = 
-				break
+			line = f1.readline()
+			if(parseLineFromWord(line) == word):
+				dictPostings[word] = createPostingListFromStr(getStrPostingsFromLine(line))
+	
 
 #Read word from line
 def parseLineForWord(str1):
-	#Parse the word from the line
+	""" Parse the word from the line """
 	#EX: Word=aaainji:Postings=[['DocID=2085', 'Pos=[16772, 16791]', 'Count=2', 'TDIF=2.0']]
 	#	 return aaainji
 	#	 [5:] , read up to :
 	indexColon = word.find(':')
 	return str1[5:indexColon]
 
-def getListPostingsFromLine(str1):
+def getStrPostingsFromLine(str1):
+	""" Get the List of Postings from Line """
 	#Parse the posting from the line
 	#EX: Word=aaainji:Postings=[['DocID=2085', 'Pos=[16772, 16791]', 'Count=2', 'TDIF=2.0']]
 	#	 return [['DocID=2085', 'Pos=[16772, 16791]', 'Count=2', 'TDIF=2.0']]
@@ -38,7 +52,25 @@ def getListPostingsFromLine(str1):
 	return str1[indexColon + len("Postings="):]
 
 def createPostingListFromStr(strPosting):
-
+	""" Construct a Posting List from the line string """
 	postingsList = list()
-	while True:
-		indexColon = str1.find("'DocID")
+	while strPosting.find("DocID") != -1:
+		tempDocList = []
+		indexColon = strPosting.find("DocID")
+		strPosting = strPosting[indexColon:]
+		tempDocList.append(strPosting[0:strPosting.find("'")])
+		
+		indexColon = strPosting.find("Pos=")
+		strPosting = strPosting[indexColon:]
+		tempDocList.append(strPosting[0:strPosting.find("'")])
+		
+		indexColon = strPosting.find("Count=")
+		strPosting = strPosting[indexColon:]
+		tempDocList.append(strPosting[0:strPosting.find("'")])
+		
+		indexColon = strPosting.find("TDIF=")
+		strPosting = strPosting[indexColon:]
+		tempDocList.append(strPosting[0:strPosting.find("'")])
+		
+		postingsList.append(tempDocList)
+	return postingsList
